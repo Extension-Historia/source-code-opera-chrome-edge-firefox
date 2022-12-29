ef1296ecebb082a27a026623bf64c3b66834219a
Lina Lerch <64536935+linalerch@users.noreply.github.com>
2022-12-29 12:36:07 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
     $('#example').DataTable({
         paging: false,
         ordering: false,
-        info: false,
-  			"language": {
-    					"search": "",
-              "searchPlaceholder": "Search..."
+        info: true,
+  	"language": {
+		"search": "",
+		"searchPlaceholder": "Search...",
+		"info": "&nbsp;&nbsp;Showing _TOTAL_ from _MAX_ entries",
+		"infoFiltered": "",
+		"infoEmpty": ""
   			},
+        "sDom": '<"top"<"actions">lfpi<"clear">><"clear">rt<"bottom">'
 
     });
 });
