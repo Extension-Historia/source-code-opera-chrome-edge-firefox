1b5ed53e5b9857a5b9a4318957fbfc54e7e40cb1
Lina Lerch <64536935+linalerch@users.noreply.github.com>
2023-06-07 15:34:27 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,74 @@
 $(document).ready(function () {
     $('#example').DataTable({
-        paging: false,
-        ordering: false,
-        info: true,
-  	"language": {
-		"search": "",
+    	 "ajax": 'data.json',
+       columns: [
+      {
+        orderable: false,
+        data: null,
+        "render": function(data, type, full, meta) {
+          return "<a href=\"" + full.link + "\" target=\"_blank\">" + full.title + " " + full.language + "<br>" + "<p>" + full.description + "</p>" + "</a>";
+        }
+      },
+      {
+        data: 'title',
+        title: 'Title'
+      },
+      {
+        data: 'description',
+        title: 'Description'
+      },
+      {
+        data: 'language',
+        title: 'Language'
+      },
+      {
+        data: 'link',
+        title: 'Link'
+      }
+    ],
+    columnDefs: [{
+        target: 0,
+        visible: true,
+      },
+      {
+        target: 1,
+        visible: false,
+      },
+      {
+        target: 2,
+        visible: false,
+      },
+      {
+        target: 3,
+        visible: false,
+      },
+      {
+        target: 4,
+        visible: false,
+      }
+    ],
+    paging: false,
+    ordering: false,
+    info: true,
+    "language": {
+    "search": "",
 		"searchPlaceholder": "Search...",
-		"info": "&nbsp;&nbsp;Showing _TOTAL_ from _MAX_ entries",
-		"infoFiltered": "",
-		"infoEmpty": ""
-  			},
-        "sDom": '<"top"<"actions">lfpi<"clear">><"clear">rt<"bottom">'
+      "info": "Showing _TOTAL_ of _MAX_ entries",
+      "infoFiltered": "",
+      "infoEmpty": ""
+    },
+        dom: 'ip'
 
     });
+});
+
+$(document).ready(function() {
+
+  var table = $('#example').DataTable();
+  
+  oTable = $('#example').DataTable();
+  $('#custom_searchbox').keyup(function() {
+    oTable.search($(this).val()).draw();
+  })
+
 });
